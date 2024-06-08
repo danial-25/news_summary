6875acca05e45cf3eec3f2727f9351424f442fa8
Danial Sakhpantayev <danial.sakhpantayev@gmail.com>
2024-06-08 19:31:04 +0500

    change of news file

--- news_script.py
+++ news_script.py
@@ -1,36 +1,42 @@
 import newsapi
 from newsapi.newsapi_client import NewsApiClient
-newsapi=NewsApiClient(api_key='a2b489b27aee49b6a39c44b9fd95428c')
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+NEWS_API = os.getenv("NEWS_API")
+newsapi = NewsApiClient(api_key=NEWS_API)
+
 
 def get_news(categories=None):
-    news=[]
+    news = []
     if categories is None:
-        newsapi=NewsApiClient(api_key='a2b489b27aee49b6a39c44b9fd95428c')
+        newsapi = NewsApiClient(api_key=NEWS_API)
 
         top_headlines = newsapi.get_top_headlines(
-                                                sources='bbc-news,the-verge',
-                                                page=3,
-                                                language='en',
-                                                )
-        return top_headlines['articles'][:3]
+            sources="bbc-news,the-verge",
+            page=3,
+            language="en",
+        )
+        return top_headlines["articles"][:3]
     else:
-        newsapi=NewsApiClient(api_key='a2b489b27aee49b6a39c44b9fd95428c')
+        newsapi = NewsApiClient(api_key=NEWS_API)
 
         for category in categories:
             if category is not None:
                 top_headlines = newsapi.get_top_headlines(
-                                                        page=3,
-                                                        category=category,
-                                                        language='en',
-                                                        )
-                news.append(top_headlines['articles'][:3])
-            else: 
-                newsapi=NewsApiClient(api_key='a2b489b27aee49b6a39c44b9fd95428c')
+                    page=3,
+                    category=category,
+                    language="en",
+                )
+                news.append(top_headlines["articles"][:3])
+            else:
+                newsapi = NewsApiClient(api_key=NEWS_API)
 
                 top_headlines = newsapi.get_top_headlines(
-                                                sources='bbc-news,the-verge',
-                                                page=3,
-                                                language='en',
-                                                )
-                news.append(top_headlines['articles'][:3])
+                    sources="bbc-news,the-verge",
+                    page=3,
+                    language="en",
+                )
+                news.append(top_headlines["articles"][:3])
         return news
